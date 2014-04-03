c35aff90b43e5daa2a41f205e9cadb48d237a97f
Trendi <jessica.zangari.90@gmail.com>
2014-04-03 16:19:16 +0200

    Rule class

---
+++ DlvGrounder/src/statement/Rule.cpp
@@ -0,0 +1,8 @@
+/*
+ * Rule.cpp
+ *
+ *  Created on: 03/apr/2014
+ *      Author: Jessica
+ */
+
+#include "Rule.h"
---
+++ DlvGrounder/src/statement/Rule.h
@@ -0,0 +1,36 @@
+/*
+ * Rule.h
+ *
+ *  Created on: 03/apr/2014
+ *      Author: Jessica
+ */
+
+#ifndef RULE_H_
+#define RULE_H_
+
+#include <vector>
+#include "../atom/Atom.h"
+
+using namespace std;
+
+class Rule {
+public:
+	Rule();
+
+	const vector<Atom*>& getBody() const {return body;}
+	void setBody(const vector<Atom*>& body) {this->body = body;}
+	const vector<Atom*>& getHead() const {return head;}
+	void setHead(const vector<Atom*>& head) {this->head = head;}
+
+	bool isAStrongConstraint(){return head.empty();}
+	bool isAFact(){return body.empty() && head.size()==1;}
+
+	void addInHead(Atom* a){head.push_back(a);};
+	void addInBody(Atom* a){body.push_back(a);};
+
+private:
+	vector<Atom*> head;
+	vector<Atom*> body;
+};
+
+#endif /* RULE_H_ */
