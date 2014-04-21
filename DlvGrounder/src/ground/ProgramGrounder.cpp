68d76e2defec808998c51e9c277117f2e39c87a6
dave90 <ddfusca@gmail.com>
2014-04-21 23:22:07 +0200

    Explode

--- DlvGrounder/src/ground/ProgramGrounder.cpp
+++ DlvGrounder/src/ground/ProgramGrounder.cpp
@@ -7,6 +7,8 @@
 
 #include "ProgramGrounder.h"
 
+#include <list>
+
 void ProgramGrounder::ground() {
 	statementDependency->createDependencyGraph(predicateTable);
 	statementDependency->createComponentGraph();
@@ -17,36 +19,10 @@ void ProgramGrounder::ground() {
 
 void ProgramGrounder::groundRule(Rule* r) {
 
-//ONLY FIRST NO ANONIMUS
 	//variable,value
-	unordered_map<unsigned long,unsigned long> bound;
+	unordered_map<unsigned long, unsigned long> bound;
+	list<unsigned long> id_match;
 
-	for (auto body_it = r->getBeginBody(); body_it != r->getEndBody(); body_it++) {
-
-		Atom *atom=*body_it;
-		vector<unsigned long> bounds_,bind_,values_,result_;
-		//FOR each term calculate bound & bind
-		for(int i=0;i<predicateTable->getPredicate(atom->getPredicate())->getArity();i++){
-			//calculate bound
-			auto it=bound.find(atom->getTerms()[i]);
-			if(it!=bound.end()){
-				bounds_.push_back(i);
-				values_.push_back(it->second);
-			}else{
-				bind_.push_back(i);
-			}
-		}
-		instancesTable->getInstance(atom->getPredicate())->getIndex()->firstMatch(bounds_,values_,bind_,result_);
-		for(int i=0;i<result_.size();i++){
-			bound.insert({bind_[i],result_[i]});
-		}
-
-	}
-
-	for(auto it:bound){
-		cout<<IdsManager::getStringStrip(IdsManager::TERM_ID_MANAGER,it.first)<<"="<<IdsManager::getStringStrip(IdsManager::TERM_ID_MANAGER,it.second)<<"  ";
-	}
-	cout<<endl;
 
 }
 
