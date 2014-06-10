3f978a885616e20b3b8bf65068d66b2e6c4f53a5
dave90 <ddfusca@gmail.com>
2014-06-10 16:02:49 +0200

    Add typedef in class

--- DlvGrounder/src/utility/IndexDefinition.h
+++ DlvGrounder/src/utility/IndexDefinition.h
@@ -8,7 +8,11 @@
 #ifndef INDEXDEFINITION_H_
 #define INDEXDEFINITION_H_
 
-typedef unsigned int index_object;
+#include <cstdint>
+
+using namespace std;
+
+typedef uint_fast8_t  index_object;
 
 class IndexDefinition{};
 
