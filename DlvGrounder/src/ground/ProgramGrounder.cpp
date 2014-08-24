16323aefdab930d520c4c1b7235ad0006993801c
Trendi <jessica.zangari@gmail.com>
2014-08-24 12:09:13 +0200

    Constraint print

--- DlvGrounder/src/ground/ProgramGrounder.cpp
+++ DlvGrounder/src/ground/ProgramGrounder.cpp
@@ -374,7 +374,9 @@ bool ProgramGrounder::printGroundRule(Rule *r,map_index_index& var_assign,bool i
 		}
 	}
 
-	if((added && groundRule->getSizeHead()==1) || (groundRule->getSizeHead()>1 && groundedRule.addRule(groundRule)))
+	if((added && groundRule->getSizeHead()==1) // If is a new fact
+			|| (groundRule->getSizeHead()>1 && groundedRule.addRule(groundRule)) // If is a disjunctive rule
+			|| (groundRule->getSizeHead()==0 && groundedRule.addRule(groundRule))) // If is a constraint
 	 	groundRule->print();
 	return added;
 }
